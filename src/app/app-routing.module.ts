56929abb01d345a4e1d3d73001881826faf3f81e
mejri chaima <https://github.com/mejriChaima>
2022-10-31 12:10:20 +0100

    integration de deux templates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { FrontLayoutComponent } from './layouts/front-layout/front-layout.component';
+import { LayoutsModule } from './layouts/layouts.module';
+import { UserLayoutComponent } from './layouts/user-layout/user-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:FrontLayoutComponent},
+
+  {path:'admin',component:AdminLayoutComponent,
+   children:[
+    {path:'dashboard',loadChildren:()=>import('./views/admin/dashbord/dashbord.module').then(m=>m.DashbordModule)},
+    {path:'reclamation',loadChildren:()=>import('./views/admin/reclamation/reclamation.module').then(m=>m.ReclamationModule)}
+   ]},
+  {path:"user",component:UserLayoutComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+  RouterModule.forRoot(routes),
+  LayoutsModule
+],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ src/app/layouts/layouts.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AdminLayoutComponent } from './admin-layout/admin-layout.component';
+import { UserLayoutComponent } from './user-layout/user-layout.component';
+import { FrontLayoutComponent } from './front-layout/front-layout.component';
+import { RouterModule } from '@angular/router';
+
+
+
+@NgModule({
+  declarations: [
+    AdminLayoutComponent,
+    UserLayoutComponent,
+    FrontLayoutComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+  ]
+})
+export class LayoutsModule { }
