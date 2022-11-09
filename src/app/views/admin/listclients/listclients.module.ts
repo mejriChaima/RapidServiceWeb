dab23cf7d1fab9b267f1d04b5009b954f65b3159
Elife-Beja-100 <Elife-Beja-100@Elife.local>
2022-11-09 17:14:24 +0100

    update by ichrak

---
+++ src/app/views/admin/listclients/listclients.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ListclientsRoutingModule } from './listclients-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ListclientsRoutingModule
+  ]
+})
+export class ListclientsModule { }
